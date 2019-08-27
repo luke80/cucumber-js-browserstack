8d1ba685a6606e7e19b313547a3c3a8b79cc605c
Luke Rebarchik <myfavoriteluke@gmail.com>
2019-08-27 10:57:07 -0500

    Fix bug with click waiting

--- tests/features/steps/click-steps.js
+++ tests/features/steps/click-steps.js
@@ -2,5 +2,5 @@ const { When } = require("cucumber");
 const { getElement } = require('../../lib/stepsGetters.js');
 
 When(/^I click on ['"]?(.+?)['"]?$/, async function (selectionDescription) {
-  (await getElement(selectionDescription, this.driver)).click();
+  return await (await getElement(selectionDescription, this.driver)).click();
 });
