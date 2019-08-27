0905f86dbfa67ed3050b019cad54b2180fee3a90
Luke Rebarchik <lr223@ntrs.com>
2019-08-27 11:00:17 -0500

    Fix bug with typing (promise chains stink)

--- tests/features/steps/type-steps.js
+++ tests/features/steps/type-steps.js
@@ -1,6 +1,6 @@
 const { Then } = require("cucumber");
-const { getSelectorFromDescription } = require('../../lib/stepsGetters.js');
+const { getElement } = require('../../lib/stepsGetters.js');
 
 Then(/^I type ['"]?(.+?)['"]? into ['"]?(.+?)['"]?$/, async function (typeString, selectionDescription) {
-  (await getElement(selectionDescription, this.driver)).sendKeys(typeString);
+  return await (await getElement(selectionDescription, this.driver)).sendKeys(typeString);
 });
