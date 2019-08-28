d5d209446aa2172f057a6e9a27035da790329bcd
Luke Rebarchik <lr223@ntrs.com>
2019-08-28 16:32:46 -0500

    Move screenshot to after typing (was before)

--- tests/features/steps/action/type.js
+++ tests/features/steps/action/type.js
@@ -5,5 +5,7 @@ addSelector(/^(Enter your .+?\.?)$/i, 'input[placeholder=\'%s\']');
 addSelector(/^(Search the web\.?)$/i, 'input[name=\'q\']');
 
 Then(/^I type ['"]?(.+?)['"]? into ['"]?(.+?)['"]?$/, async function (typeString, selectionDescription) {
-  return await (await getElement(selectionDescription, this.driver)).sendKeys(typeString);
+  let typing = await (await getElement(selectionDescription, this.driver, false)).sendKeys(typeString);
+  await this.driver.takeScreenshot(true);
+  return typing;
 });
