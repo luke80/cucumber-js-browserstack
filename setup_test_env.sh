894f0225a647a46b97143465660245606ac03d4e
Luke Rebarchik <lr223@ntrs.com>
2019-08-28 13:30:31 -0500

    Update the setup script for .env

--- setup_test_env.sh
+++ setup_test_env.sh
@@ -1,39 +1,50 @@
 #!/bin/bash
 
-# BROWSERSTACK_USERNAME
-if [ ! -n "${BROWSERSTACK_USERNAME}" ]; then
-  echo -n "BrowserStack User: ";
-  read BROWSERSTACK_USERNAME
-  export BROWSERSTACK_USERNAME
+DODOTENV=${1:-true}
+DOTENVPATH='./'
+if [ -d "tests" ]; then
+  DOTENVPATH='./tests/'
 fi
 
-# BROWSERSTACK_ACCESS_KEY
-if [ ! -n "${BROWSERSTACK_ACCESS_KEY}" ]; then
-  echo -n "BrowserStack Key: ";
-  read BROWSERSTACK_ACCESS_KEY
-  export BROWSERSTACK_ACCESS_KEY
-fi
-
-# SELENIUM_PROMISE_MANAGER
-if [ ! -n "${SELENIUM_PROMISE_MANAGER}" ]; then
-  echo "Disabling Selenium promise manager.";
-  export SELENIUM_PROMISE_MANAGER=0
-fi
-
-# PROXY
-if [ ! -n "${PROXY}" ]; then
-  echo -n "Proxy you want to use: ";
-  read PROXY
-  if [ ! -n "${PROXY}" ]; then
-    export PROXY
+if [ "${DODOTENV}" = "true" ]; then
+  if [ -f "${DOTENVPATH}.env" ]; then
+    echo "There is already a .env file at your path (${DOTENVPATH}.env). Delete it or move it before running this script." >&2
+    exit 1
   fi
+  echo -e "Creating .env file with your input\n"
 fi
 
-# TESTING_HOST
-if [ ! -n "${TESTING_HOST}" ]; then
-  echo -n "Host you want to test: ";
-  read TESTING_HOST
-  if [ ! -n "${TESTING_HOST}" ]; then
-    export TESTING_HOST
+commands=() # initialize the variable setting commands
+variables=("BROWSERSTACK_USERNAME=%s" "BROWSERSTACK_ACCESS_KEY=%s" "TESTING_HOST=%s" "SELENIUM_PROMISE_MANAGER=0" "(optional) PROXY=%s")
+for v in "${variables[@]}"
+do
+  name=$(echo "${v}" | cut -d= -f1)
+  variable=${name/\(optional\) /}
+  if [[ "${v}" == *"=%s"* ]]; then
+    #prompt="${name//_/ }"
+    echo -n "${name}="
+    read input
+    if [[ "${v}" != *"(optional)"* ]] || ([[ "${v}" == *"(optional)"* ]] && [ -n "${input}" ]); then
+      v=${v/\(optional\) /}
+      v=${v/\%s/$input}
+      commands+=(${v})
+    fi
+  else
+    commands+=(${v})
+  fi
+done
+
+if [ ${#commands[@]} -gt 0 ]; then
+  if [ "${DODOTENV}" = "true" ]; then
+    command=$(printf "\n%s" "${commands[@]}")
+    command=${command:1} # remove the leading '\n'
+    echo -e "${command}" > "${DOTENVPATH}.env"
+  else
+    glue=" && export "
+    command=$(printf "${glue}%s" "${commands[@]}")
+    command=${command:${#glue}} # remove the leading glue
+
+    echo "Run this command chain:"
+    echo "export ${command}"
   fi
 fi
